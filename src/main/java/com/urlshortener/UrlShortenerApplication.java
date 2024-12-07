4d8a15f3ac64cc399000277a4119972403b0390b
André Cruz <acruztalavera@gmail.com>
2024-12-07 15:52:12 -0500

    UrlRepsonseDto and consul config

--- src/main/java/com/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/urlshortener/UrlShortenerApplication.java
@@ -3,9 +3,11 @@ package com.urlshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
 public class UrlShortenerApplication {
   public static void main(String[] args) {
     SpringApplication.run(UrlShortenerApplication.class, args);
